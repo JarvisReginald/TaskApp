aa8aa88883e5c0267d4fedb194bd465090948fde
SkyBoi <79283649+SkyBoiMC@users.noreply.github.com>
2023-11-07 11:03:28 -0500

    finished the box

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import { useState } from 'react'
 import './App.css'
 
-function Box() {
+function Box({ name, job, title, time }) {
   return (
     <div className='ProjectBox'>
-
+      <h1>ETemple</h1>
+      <h2>Editing</h2>
+      <p>How Minecraft Modding Became a Business</p>
+      <p>50 days left</p>
     </div>
   )
 }
 
+function Bar()
+{
+  return (
+    <div className='ProgressBar'>
+      <label>
+
+      </label>
+    </div>
+  )
+}
 
 function App() {
 
   return (
-    <Box />
+    <div>
+      <Box name="ETemple" job="Editing" title="How Minecraft Modding Became a Business" time="50 days left" />
+      <Bar />
+    </div>
+
   )
  
 }
